a4634d4213565650f929e2dfbf3714633b067652
Bertrand Kerautret <bertrand.kerautret@univ-lyon2.fr>
2026-06-14 17:12:30 +0200

    send tools key

--- MailWeave/SendTools.swift
+++ MailWeave/SendTools.swift
@@ -7,13 +7,14 @@
 
 
 import Cocoa
+import Carbon.HIToolbox
 
 // use the Standard CMD-SHIFT-D to send the email from interface
 func sendShortcut() {
     let source = CGEventSource(stateID: .hidSystemState)
     let keyDown = CGEvent(
         keyboardEventSource: source,
-        virtualKey: 2, // D
+        virtualKey: CGKeyCode(kVK_ANSI_D), // D
         keyDown: true
     )
     keyDown?.flags = [.maskCommand, .maskShift]
@@ -21,7 +22,7 @@ func sendShortcut() {
 
     let keyUp = CGEvent(
         keyboardEventSource: source,
-        virtualKey: 2,
+        virtualKey: CGKeyCode(kVK_ANSI_D),
         keyDown: false
     )
 
